e030633d2e34196d2c59ab2c570f040f6d4745bb
tiffanyposs <tiffanyposs@gmail.com>
2015-01-09 16:13:26 -0500

    villain updates

--- rpg.rb
+++ rpg.rb
@@ -1,11 +1,8 @@
 #RPG
 
-
-
-
 puts "What is your name?"
 
-	hero = {"name"=> "", "health_points"=> 10}
+	hero = {"name"=> "", "hit_points"=> 10, "hit_chance"=> 80}
 
 	hero[:name] = gets.chomp.capitalize!
 
@@ -13,32 +10,35 @@ puts "#{hero[:name]} enters the forest!"
 
 puts "Hi #{hero[:name]}! Prepare for battle!"
 
-puts "... will #{hero[:name]} fight or flee?"
-
-fight_flee = gets.chomp.downcase
+class Villain 
+	def initialize(name)
+		@name = name
+		@hit_points = 6
+		@hit_chance = 60
+		# @strength = 4
+		# @defense = 4
+	end
+end
 
 def fight
-
+	puts "#{hero[:name]} attacks!"
 	#########
-
 end
 
 def flee
-
 	######
-
 end
 
-question = true
+fight_question = true
 
-while question
+while fight_question == true
+	puts "... will #{hero[:name]} fight or flee?"
+	fight_flee = gets.chomp.downcase
 	if fight_flee == "fight"
 		fight()
-		question = false
+		fight_question = false
 	elsif fight_flee == "flee"
 		flee()
-		question = false
-	else
-		puts "Type fight or flee"
+		fight_question = false
 	end
 end
