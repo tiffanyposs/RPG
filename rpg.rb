75362eb58faf3b11dab3ce9ac36b592fa1723751
DavideDaniel <davidedaniel@gmail.com>
2015-01-09 17:26:11 -0500

    building an array

--- rpg.rb
+++ rpg.rb
@@ -2,43 +2,52 @@
 
 puts "What is your name?"
 
-	hero = {"name"=> "", "hit_points"=> 10, "hit_chance"=> 80}
+hero = {"name"=> "", "hit_points"=> 10, "hit_chance"=> 80}
 
-	hero[:name] = gets.chomp.capitalize!
+hero[:name] = gets.chomp.capitalize!
 
 puts "#{hero[:name]} enters the forest!"
 
 puts "Hi #{hero[:name]}! Prepare for battle!"
 
-class Villain 
-	def initialize(name)
-		@name = name
-		@hit_points = 6
-		@hit_chance = 60
-		# @strength = 4
-		# @defense = 4
-	end
+class Villain
+  def initialize(name)
+    @name = name
+    @hit_points = 6
+    @hit_chance = 60
+    # @strength = 4
+    # @defense = 4
+  end
 end
 
+shrek = Villain.new("Shrek")
+
 def fight
-	puts "#{hero[:name]} attacks!"
-	#########
+  puts "#{hero[:name]} attacks!"
+  
+  if rand(1..100) <= 80
+  	baddie
+
+
+  ######
+
 end
 
 def flee
-	######
+  ######
 end
 
 fight_question = true
 
 while fight_question == true
-	puts "... will #{hero[:name]} fight or flee?"
-	fight_flee = gets.chomp.downcase
-	if fight_flee == "fight"
-		fight()
-		fight_question = false
-	elsif fight_flee == "flee"
-		flee()
-		fight_question = false
-	end
+  puts "... will #{hero[:name]} fight or flee?"
+  fight_flee = gets.chomp.downcase
+  if fight_flee == "fight"
+    fight()
+    fight_question = false
+  elsif fight_flee == "flee"
+    flee()
+    fight_question = false
+  end
 end
+
